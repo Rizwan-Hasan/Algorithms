763b3d7a21469543d4aa6db6807ea502d047a1a7
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2020-06-16 22:32:48 +0600

    Update base62.py

--- BASE62/base62.py
+++ BASE62/base62.py
@@ -1,3 +1,7 @@
+import os
+os.system('clear')
+
+
 def encodeToBase62(decimal: int):
     if decimal <= 0:
         return 0
@@ -18,11 +22,28 @@ def encodeToBase62(decimal: int):
 
 def encode(string: str):
     if string:
-        return encodeToBase62(int.from_bytes(bytes(string, 'utf-8'), "big"))
+        decimal: int = int.from_bytes(bytes(string, 'utf-8'), 'big')
+        return encodeToBase62(decimal)
     else:
         return 'Invalid String!'
 
 
+def decode(string: str):
+    alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base**power)
+        idx += 1
+
+    return num.to_bytes((num.bit_length() + 7) // 8, 'big').decode('utf-8')
+
+
 if __name__ == "__main__":
-    string: str = 'Hello World!'
+    string: str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     print(encode(string))
+    print(decode(encode(string)))
